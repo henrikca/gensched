297937ffbc19f0be98cacfaa69c10e770384d903
Mille Bessö <millebesso@gmail.com>
2020-07-20 22:15:51 +0200

    Renaming and removing linting errors

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ from math import sqrt
 import sys
 from datetime import datetime
 
-# TARGET = [0, 0, 1, 2, 3, 5, 5, 5, 6, 3, 0]
-TARGET = [0, 5, 10, 15, 15, 20, 15, 15, 10, 10, 25, 30, 20, 10, 20, 25, 10, 10, 0]
+TARGET = [0, 0, 1, 2, 3, 5, 5, 5, 6, 3, 1]
+# TARGET = [0, 5, 10, 15, 15, 20, 15, 15, 10, 10, 25, 30, 20, 10, 20, 25, 10,
+#           10, 0]
 
 MAX_SHIFT_LENGTH = 8
 MIN_SHIFT_LENGTH = 3
 
 MUTATION_RATE = 0.25
-POPULATION = 1000
+POPULATION = 100
 GENERATIONS = 1000
-NEGATIVE_PENALTY_FACTOR = 1.5
+NEGATIVE_PENALTY_FACTOR = 1
 
 
 def run():
@@ -23,10 +24,10 @@ def run():
     for i in range(GENERATIONS):
         population = [copy_schedule(schedule) for x in range(POPULATION)]
         mutate_population(population)
-        current_rms = rms(schedule)
+        current_rms = get_rms(schedule)
 
         new_schedule = get_best_schedule(population)
-        new_rms = rms(new_schedule)
+        new_rms = get_rms(new_schedule)
         if new_rms < current_rms:
             schedule = new_schedule
             print(f'Generation {i}')
@@ -43,7 +44,7 @@ def get_best_schedule(population):
     best_rms = float(sys.maxsize)
     best_schedule = None
     for schedule in population:
-        current_rms = rms(schedule)
+        current_rms = get_rms(schedule)
         if current_rms < best_rms:
             best_rms = current_rms
             best_schedule = schedule
@@ -80,7 +81,7 @@ def output_current(schedule):
     print(get_schedule_as_string(schedule))
     print(format_array(get_slot_sum(schedule)))
     print(format_array(TARGET))
-    print(f'RMS: {rms(schedule)}')
+    print(f'RMS: {get_rms(schedule)}')
     print()
 
 
@@ -90,8 +91,8 @@ def copy_schedule(schedule):
 
 def initialize():
     schedule = []
-    # agents = 1
-    agents = max(TARGET)
+    agents = 1
+    # agents = max(TARGET)*2
     for i in range(agents):
         shift = create_agent_shift()
         schedule.append(shift)
@@ -111,7 +112,8 @@ def create_agent_shift():
     slots = len(TARGET)
     start = randint(0, slots - MIN_SHIFT_LENGTH)
     length = randint(MIN_SHIFT_LENGTH, min(MAX_SHIFT_LENGTH, slots-start))
-    shift = [1 if x >= start and x <= start+length-1 else 0 for x in range(slots)]
+    shift = [
+        1 if x >= start and x <= start+length-1 else 0 for x in range(slots)]
     return shift
 
 
@@ -130,12 +132,13 @@ def format_array(array):
     return format_string.format(*array)
 
 
-def rms(schedule):
+def get_rms(schedule):
     slot_sum = get_slot_sum(schedule)
     square_diff = []
     for i in range(len(TARGET)):
         if slot_sum[i] < TARGET[i]:
-            square_diff.append((NEGATIVE_PENALTY_FACTOR * (slot_sum[i] - TARGET[i]))**2)
+            square_diff.append(
+                (NEGATIVE_PENALTY_FACTOR * (slot_sum[i] - TARGET[i]))**2)
         else:
             square_diff.append((slot_sum[i] - TARGET[i])**2)
     # square_diff = [(slot_sum[i] - TARGET[i])**2 for i in range(len(TARGET))]
